15bf6f590e0a7da12ea97689b1cb1ebdbe1b6757
askrepps <askrepps@gmail.com>
2021-12-07 00:18:42 -0500

    Added solutions for day 7

---
+++ src/main/kotlin/com/askrepps/advent2021/day07/Day07.kt
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Andrew Krepps
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.askrepps.advent2021.day07
+
+import java.io.File
+import kotlin.math.abs
+
+fun findOptimalAlignmentCost(startingPositions: List<Int>, calcFuelCost: (Int, Int) -> Int): Int {
+    val minPosition = startingPositions.minOrNull() ?: throw RuntimeException("Can't find min")
+    val maxPosition = startingPositions.maxOrNull() ?: throw RuntimeException("Can't find max")
+
+    var optimalFuel = Int.MAX_VALUE
+    for (alignment in minPosition..maxPosition) {
+        val fuelCost = startingPositions.sumOf { calcFuelCost(it, alignment) }
+        if (fuelCost < optimalFuel) {
+            optimalFuel = fuelCost
+        }
+    }
+
+    return optimalFuel
+}
+
+fun getPart1Answer(startingPositions: List<Int>) =
+    findOptimalAlignmentCost(startingPositions) { from, to -> abs(from - to) }
+
+fun getPart2Answer(startingPositions: List<Int>) =
+    findOptimalAlignmentCost(startingPositions) { from, to ->
+        val distance = abs(from - to)
+        distance * (distance + 1) / 2
+    }
+
+fun main() {
+    val startingPositions = File("src/main/resources/day07.txt")
+        .readText().split(",").map { it.toInt() }
+
+    println("The answer to part 1 is ${getPart1Answer(startingPositions)}")
+    println("The answer to part 2 is ${getPart2Answer(startingPositions)}")
+}
---
+++ src/test/kotlin/com/askrepps/advent2021/day07/Day07Test.kt
@@ -0,0 +1,41 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Andrew Krepps
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.askrepps.advent2021.day07
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+private const val testData = "16,1,2,0,4,2,7,1,2,14"
+
+class Day07Test {
+
+    @Test
+    fun testDay07() {
+        val lines = testData.split(",").map { it.toInt() }
+
+        assertEquals(37, getPart1Answer(lines))
+        assertEquals(168, getPart2Answer(lines))
+    }
+}
