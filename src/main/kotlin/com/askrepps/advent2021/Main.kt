548b8e046d17a8be342f240fa3be92717ff3bc63
askrepps <askrepps@gmail.com>
2021-12-20 08:32:04 -0500

    Added solutions for day 20

--- src/main/kotlin/com/askrepps/advent2021/Main.kt
+++ src/main/kotlin/com/askrepps/advent2021/Main.kt
@@ -42,14 +42,15 @@ import com.askrepps.advent2021.day15.main as runDay15
 import com.askrepps.advent2021.day16.main as runDay16
 import com.askrepps.advent2021.day17.main as runDay17
 import com.askrepps.advent2021.day18.main as runDay18
-import com.askrepps.advent2021.day18.main as runDay19
+import com.askrepps.advent2021.day19.main as runDay19
+import com.askrepps.advent2021.day20.main as runDay20
 
 fun main(args: Array<String>) {
     val runners = listOf(
         ::runDay01, ::runDay02, ::runDay03, ::runDay04, ::runDay05,
         ::runDay06, ::runDay07, ::runDay08, ::runDay09, ::runDay10,
         ::runDay11, ::runDay12, ::runDay13, ::runDay14, ::runDay15,
-        ::runDay16, ::runDay17, ::runDay18, ::runDay19
+        ::runDay16, ::runDay17, ::runDay18, ::runDay19, ::runDay20
     )
 
     val day = args.firstOrNull()
