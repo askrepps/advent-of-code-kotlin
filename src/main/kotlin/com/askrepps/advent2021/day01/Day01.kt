832bd99f9c9b8b16be149772de371eba9280c22e
askrepps <askrepps@gmail.com>
2021-12-21 04:40:04 -0500

    Revert "Removed unnecessary sums from day 1 solution"
    This reverts commit b4514193b8314888ec0c8af9c3940aadc5396d85.

--- src/main/kotlin/com/askrepps/advent2021/day01/Day01.kt
+++ src/main/kotlin/com/askrepps/advent2021/day01/Day01.kt
@@ -27,13 +27,17 @@ package com.askrepps.advent2021.day01
 import com.askrepps.advent2021.util.getInputLines
 import java.io.File
 
-fun List<Int>.countIncreases(delay: Int = 1) =
-    (delay until size).count { this[it] > this[it - delay] }
+fun List<Int>.countIncreases() =
+    (1 until size).count { this[it] > this[it - 1] }
 
-fun getPart1Answer(depths: List<Int>) = depths.countIncreases()
+fun List<Int>.getWindowSums(windowSize: Int) =
+    (windowSize..size).map { subList(it - windowSize, it).sum() }
 
-// b + c + d > a + b + c <=> d > a
-fun getPart2Answer(depths: List<Int>) = depths.countIncreases(delay = 3)
+fun getPart1Answer(depths: List<Int>) =
+    depths.countIncreases()
+
+fun getPart2Answer(depths: List<Int>) =
+    depths.getWindowSums(windowSize = 3).countIncreases()
 
 fun main() {
     val depths = File("src/main/resources/day01.txt")
