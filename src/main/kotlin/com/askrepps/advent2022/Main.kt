f7d0ea2d4347cc9373ccf4ba8d02530085c900d3
askrepps <askrepps@gmail.com>
2022-12-06 00:14:07 -0500

    Add solutions for day 6

--- src/main/kotlin/com/askrepps/advent2022/Main.kt
+++ src/main/kotlin/com/askrepps/advent2022/Main.kt
@@ -31,9 +31,11 @@ import com.askrepps.advent2022.day02.main as runDay02
 import com.askrepps.advent2022.day03.main as runDay03
 import com.askrepps.advent2022.day04.main as runDay04
 import com.askrepps.advent2022.day05.main as runDay05
+import com.askrepps.advent2022.day06.main as runDay06
 
 private val runners = mapOf(
-     1 to ::runDay01,  2 to ::runDay02,  3 to ::runDay03,  4 to ::runDay04,  5 to ::runDay05
+     1 to ::runDay01,  2 to ::runDay02,  3 to ::runDay03,  4 to ::runDay04,  5 to ::runDay05,
+     6 to ::runDay06
 )
 
 fun runDay(dayNumber: Int) {
