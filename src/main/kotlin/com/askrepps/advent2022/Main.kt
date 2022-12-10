cbf4918cfa97b683d1950c6922f640efea829d2a
askrepps <askrepps@gmail.com>
2022-12-10 00:30:41 -0500

    Add solutions for day 10

--- src/main/kotlin/com/askrepps/advent2022/Main.kt
+++ src/main/kotlin/com/askrepps/advent2022/Main.kt
@@ -35,10 +35,11 @@ import com.askrepps.advent2022.day06.main as runDay06
 import com.askrepps.advent2022.day07.main as runDay07
 import com.askrepps.advent2022.day08.main as runDay08
 import com.askrepps.advent2022.day09.main as runDay09
+import com.askrepps.advent2022.day10.main as runDay10
 
 private val runners = mapOf(
      1 to ::runDay01,  2 to ::runDay02,  3 to ::runDay03,  4 to ::runDay04,  5 to ::runDay05,
-     6 to ::runDay06,  7 to ::runDay07,  8 to ::runDay08,  9 to ::runDay09
+     6 to ::runDay06,  7 to ::runDay07,  8 to ::runDay08,  9 to ::runDay09, 10 to ::runDay10
 )
 
 fun runDay(dayNumber: Int) {
---
+++ src/main/kotlin/com/askrepps/advent2022/day10/Day10.kt
@@ -0,0 +1,109 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Andrew Krepps
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.askrepps.advent2022.day10
+
+import com.askrepps.advent2022.util.getInputLines
+import java.io.File
+import kotlin.math.abs
+
+sealed class Instruction(val cycles: Int)
+
+object NoOp : Instruction(cycles = 1)
+
+data class AddX(val value: Int) : Instruction(cycles = 2)
+
+fun String.toInstruction() =
+    if (this == "noop") {
+        NoOp
+    } else {
+        AddX(split(" ").last().toInt())
+    }
+
+fun runProgram(instructions: List<Instruction>, onCycleFinished: (Int, Int) -> Unit) {
+    var programCounter = 0
+    var register = 1
+    var cycle = 0
+    var skipCount = 0
+    var pendingValue = 0
+    while (programCounter < instructions.size) {
+        cycle++
+        if (skipCount > 0) {
+            skipCount--
+        } else {
+            register += pendingValue
+            val instruction = instructions[programCounter]
+            skipCount = instruction.cycles - 1
+            pendingValue =
+                when (instruction) {
+                    NoOp -> 0
+                    is AddX -> instruction.value
+                }
+            programCounter++
+        }
+        onCycleFinished(cycle, register)
+    }
+}
+
+fun getTotalSignalStrength(instructions: List<Instruction>): Int {
+    var totalStrength = 0
+    runProgram(instructions) { cycle, register ->
+        if ((cycle - 20) % 40 == 0) {
+            totalStrength += cycle * register
+        }
+    }
+    return totalStrength
+}
+
+fun getRenderedImage(instructions: List<Instruction>): String {
+    val sb = StringBuilder()
+    runProgram(instructions) { cycle, register ->
+        val renderColumn = (cycle - 1) % 40
+        val pixel =
+            if (abs(register - renderColumn) <= 1) {
+                '#'
+            } else {
+                '.'
+            }
+        sb.append(pixel)
+        if (renderColumn == 39) {
+            sb.append('\n')
+        }
+    }
+    return sb.toString().trim()
+}
+
+fun getPart1Answer(instructions: List<Instruction>) =
+    getTotalSignalStrength(instructions)
+
+fun getPart2Answer(instructions: List<Instruction>) =
+    getRenderedImage(instructions)
+
+fun main() {
+    val instructions = File("src/main/resources/2022/day10.txt")
+        .getInputLines().map { it.toInstruction() }
+
+    println("The answer to part 1 is ${getPart1Answer(instructions)}")
+    println("The answer to part 2 is \n${getPart2Answer(instructions)}")
+}
